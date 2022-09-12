b53dfa7b2bfe3ab422e3672c51df5e3e19e49f8f
seedevk8s <seedevk8s@gmail.com>
2022-09-12 20:37:43 +0900

    사칙연산 계산기 구현4 - 구현체 3개 추가 (리펙토링)

---
+++ src/main/java/org/example02/SubtractionOperator.java
@@ -0,0 +1,13 @@
+﻿package org.example02;
+
+public class SubtractionOperator implements NewArithmeticOperator {
+    @Override
+    public boolean supports(String operator) {
+        return "-".equals(operator);
+    }
+
+    @Override
+    public int calculate(int operand1, int operand2) {
+        return operand1 - operand2;
+    }
+}
