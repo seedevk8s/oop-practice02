bae2486564fc7604c702fa65323a8c0ea3517a75
seedevk8s <seedevk8s@gmail.com>
2022-09-12 14:50:37 +0900

    first commit

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ 사칙연산 계산기 구현
+ • 요구사항
+     • 간단한 사칙연산을 할 수 있다.
+     • 양수로만 계산할 수 있다.
+     • 나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.
+     • MVC패턴(Model-View-Controller) 기반으로 구현한다.
+ *
+ */
+public class CalculatorTest {
+
+    @Test
+    void name() {
+    }
+}
