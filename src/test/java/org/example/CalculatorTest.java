09920fa5d4b4cd2873a9296b04f2f686fbf07e2d
seedevk8s <seedevk8s@gmail.com>
2022-09-12 17:52:53 +0900

    사칙연산 계산기 구현1

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,19 +1,30 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  사칙연산 계산기 구현
  • 요구사항
-     • 간단한 사칙연산을 할 수 있다.
-     • 양수로만 계산할 수 있다.
-     • 나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.
-     • MVC패턴(Model-View-Controller) 기반으로 구현한다.
+ • 간단한 사칙연산을 할 수 있다.
+ • 양수로만 계산할 수 있다.
+ • 나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.
+ • MVC패턴(Model-View-Controller) 기반으로 구현한다.
  *
  */
 public class CalculatorTest {
 
+    /**
+     *  1 + 2 ----> Calculator
+     *    3   <----
+     */
+    @DisplayName("덧셈 연산을 수행한다.")
     @Test
-    void name() {
+    void additionTest() {
+
+        int result = Calculator.calculate(1, "+", 2);
+        assertThat(result).isEqualTo(3);
     }
 }
