22c247b640d2de8a1dd69136a8ef755adbbc3771
seedevk8s <seedevk8s@gmail.com>
2022-09-12 18:09:16 +0900

    사칙연산 계산기 구현1 - 테스트 뺄셈 추가

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,7 +4,9 @@ public class Calculator {
     public static int calculate(int operand1, String operator, int operand2) {
         if ("+".equals(operator)) {
             return operand1 + operand2;
-        }
+        } else if ("-".equals(operator))
+            return operand1 - operand2;
+
         return 0;
     }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -11,8 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  • 간단한 사칙연산을 할 수 있다.
  • 양수로만 계산할 수 있다.
  • 나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.
- • MVC패턴(Model-View-Controller) 기반으로 구현한다.
- *
+ • MVC패턴(Model-View-Controller) 기반으로 구현한다. *
  */
 public class CalculatorTest {
 
@@ -25,5 +24,12 @@ public class CalculatorTest {
     void additionTest() {
         int result = Calculator.calculate(1, "+", 2);
         assertThat(result).isEqualTo(3);
+    }
+
+    @DisplayName("뺄셈 연산을 수행한다.")
+    @Test
+    void subtractionTest() {
+        int result = Calculator.calculate(1, "-", 2);
+        assertThat(result).isEqualTo(-1);
     }
 }
