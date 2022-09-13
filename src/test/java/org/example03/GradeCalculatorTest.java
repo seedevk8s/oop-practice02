e1b9fa0c308fa9b4cbcaf888a860eb79578234b1
seedevk8s <seedevk8s@gmail.com>
2022-09-13 22:42:56 +0900

    학점계산기 구현 - first commit

---
+++ src/test/java/org/example03/GradeCalculatorTest.java
@@ -0,0 +1,21 @@
+package org.example03;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ 학점계산기 구현
+ • 요구사항
+     • 평균학점 계산 방법 = (학점수×교과목 평점)의 합계/수강신청 총학점 수
+     • 일급 컬렉션 사용
+ *
+ */
+public class GradeCalculatorTest {
+    // 학점계산기, 코스
+    // 평균학점 계산 요청 ---> '학점계산기' ---> (학점수×교과목 평점)의 합계 ---> '코스'
+    //                               ---> 수강신청 총학점 수
+
+
+    @Test
+    void calculateGradeTest() {
+    }
+}
