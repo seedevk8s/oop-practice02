fde79f7463afc5ec19274fc7b25f09ee19391d8b
seedevk8s <seedevk8s@gmail.com>
2022-09-13 23:39:30 +0900

    학점계산기 구현 - Course.java 메서드 2개 추가 완성

--- src/main/java/org/example03/Course.java
+++ src/main/java/org/example03/Course.java
@@ -3,7 +3,7 @@ package org.example03;
 public class Course {
     private final String subject;           // 과목명
     private final int credit;               // 학점
-    private final String grade;             // 성적
+    private final String grade;             // 성적(A+, A, B+,...)
 
     public Course(String subject, int credit, String grade) {
         this.subject = subject;
@@ -15,7 +15,30 @@ public class Course {
         return credit;
     }
 
-    public int getGradeToNumber() {
-        return 0;
+    public double getGradeToNumber() {
+        double grade = 0;
+        switch (this.grade) {
+            case "A+":
+                grade = 4.5;
+                break;
+            case "A":
+                grade = 4.0;
+                break;
+            case "B+":
+                grade = 3.5;
+                break;
+            case "B":
+                grade = 3.0;
+                break;
+            case "C+":
+                grade = 2.5;
+                break;
+            case "C":
+                grade = 2.0;
+                break;
+
+        }
+
+        return grade;
     }
 }
